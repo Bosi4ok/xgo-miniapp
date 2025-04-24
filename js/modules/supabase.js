0b4d426480b45543a1453ac6a758ef9ad9dbe872
Bosi4ok <113308757+Bosi4ok@users.noreply.github.com>
2025-04-24 11:21:21 +0300

    Add files via upload

---
+++ js/modules/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.7'
+
+// Замените эти значения на ваши из настроек проекта в Supabase
+const SUPABASE_URL = 'https://msstnczyshmnhjcnzjlg.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zc3RuY3p5c2htbmhqY256amxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMjI0MjUsImV4cCI6MjA2MDg5ODQyNX0.9Oa_ghFyX9qVquxokvLMSNRfQq7FzA6mQEvlsM2ZyRc'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
