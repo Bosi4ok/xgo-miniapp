4b1ab32de7a23321a38846a4ad7a6b65ca43f2a9
Bosi4ok <113308757+Bosi4ok@users.noreply.github.com>
2025-04-24 10:53:48 +0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,2 +0,0 @@
-const supabaseUrl = 'https://msstnczyshmnhjcnzjlg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zc3RuY3p5c2htbmhqY256amxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMjI0MjUsImV4cCI6MjA2MDg5ODQyNX0.9Oa_ghFyX9qVquxokvLMSNRfQq7FzA6mQEvlsM2ZyRc'
