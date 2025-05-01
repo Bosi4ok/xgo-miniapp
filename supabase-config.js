9a9c3c84e6f65446b54b33b4dea415c61f287bd5
Артем <artem@MacBook-Pro.local>
2025-05-01 17:01:34 +0300

    Fix: Rename export in supabase-config.js to supabaseClient

--- supabase-config.js
+++ supabase-config.js
@@ -4,4 +4,4 @@ import { createClient } from '@supabase/supabase-js'
 const SUPABASE_URL = 'https://msstnczyshmnhjcnzjlg.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zc3RuY3p5c2htbmhqY256amxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMjI0MjUsImV4cCI6MjA2MDg5ODQyNX0.9Oa_ghFyX9qVquxokvLMSNRfQq7FzA6mQEvlsM2ZyRc'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
