42c1bbd53211537bd46a3319827bd96d15c30fcb
Артем <artem@MacBook-Pro.local>
2025-05-01 16:59:02 +0300

    Refactor: Use npm package for supabase-js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.7'
+import { createClient } from '@supabase/supabase-js'
 
 // Замените эти значения на ваши из настроек проекта в Supabase
 const SUPABASE_URL = 'https://msstnczyshmnhjcnzjlg.supabase.co'
